9cc19afb0241603bbe07c044097984451c6ad24a
erikmjohnson <erikarisma98@gmail.com>
2019-02-15 15:56:29 -0800

    Working on Factory Function

---
+++ vehicle-class.js
@@ -0,0 +1,32 @@
+'use strict';
+
+class Vehicle {
+  constructor (name, wheels) {
+    this.name = name;
+    this.wheels = wheels;
+  }
+  drive() {
+    return 'Moving Forward';
+  }
+  stop() {
+    return 'Stopping';
+  }
+}
+
+class Car extends Vehicle {
+  constructor(name, color) {
+    super(name, 4);
+    this.color = color;
+  }
+}
+
+class Motorcycle extends Vehicle {
+  constructor (name) {
+    super(name, 2);
+  }
+  wheelie() {
+    return 'Wheee!';
+  }
+}
+
+module.exports = {Car, Motorcycle};
